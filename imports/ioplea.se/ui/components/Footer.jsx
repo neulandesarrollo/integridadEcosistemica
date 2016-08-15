76afec1251c8bea442cf54bd571d64342bde519d
Marvin Arnold <marvinmarnold@gmail.com>
2016-08-14 22:59:22 -0500

    Confirmation after stuff submit

--- imports/ioplea.se/ui/components/Footer.jsx
+++ imports/ioplea.se/ui/components/Footer.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { createContainer } from 'meteor/react-meteor-data';
 
 import ControllersCheckboxes from './ControllersCheckboxes.jsx';
 import KindsCheckboxes from './KindsCheckboxes.jsx';
 import ThingsCheckboxes from './ThingsCheckboxes.jsx';
 
-export default class Footer extends Component {
+const SHOW_SUCCESS = "showSuccess";
+
+class Footer extends Component {
   handleClick(event) {
     $('#submitModal').modal('show')
 
@@ -16,7 +19,7 @@ export default class Footer extends Component {
     event.preventDefault()
     const t = event.target;
     t.disabled = true
-    
+
     const stuff = {
       name: t.name.value,
       description: t.description.value,
@@ -51,60 +54,93 @@ export default class Footer extends Component {
         _.each(t.controllers, c => { c.checked = false })
 
         t.disabled = false;
-        $("#submitModal").modal('hide');
+        // $("#submitModal").modal('hide');
+        Session.set(SHOW_SUCCESS, true)
       }
     });
+  }
+
+  renderForm() {
+    return (
+      <div className="modal-content">
+        <div className="modal-header">
+          <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+          <h4 className="modal-title" id="myModalLabel">Recommend Stuff</h4>
+          <p>Stuff can be an app, a package, an API or anything else that adds functionality to an IoT device.</p>
+        </div>
+        <form onSubmit={this.handleSubmit.bind(this)}>
+          <div className="modal-body">
+              <h5 className='m-t-1'>Details</h5>
+              <div className="form-group">
+                <label htmlFor="name">Name</label>
+                <input type="text" className="form-control" id="name" aria-describedby="Name" />
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="description">Description</label>
+                <textarea className="form-control" id="description" rows="5"></textarea>
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="compnay">Company</label>
+                <input type="text" className="form-control" id="company" aria-describedby="company" />
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="iconUrl">Icon URL</label>
+                <input type="text" className="form-control" id="iconUrl" aria-describedby="IconURL" />
+              </div>
+
+              <h5>Compatible IoT</h5>
+              <ThingsCheckboxes />
+
+              <h5 className="m-t-1">Runs on</h5>
+              <ControllersCheckboxes />
+
+              <h5 className="m-t-1">Tags</h5>
+              <KindsCheckboxes />
+          </div>
+          <div className="modal-footer">
+            <button type="submit" className="btn btn-primary btn-lg btn-block">Submit</button>
+          </div>
+        </form>
+      </div>
+    )
+  }
+
+  handleAnother() {
+    Session.set(SHOW_SUCCESS, false)
+  }
+
+  renderSuccess() {
+    return (
+      <div className="modal-content">
+
+        <div className="modal-body">
+          <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+          <h1>Stuff Submitted</h1>
+          <h4 className='m-y-2'>Your Stuff recommendation has been successfully submitted. It will be reviewed before being made visible to others.</h4>
+
+          <button type="button" className="btn btn-lg btn-primary btn-block" aria-label="submitAnother" onClick={this.handleAnother.bind(this)}>
+            Submit Another
+          </button>
+          <button type="button" className="btn btn-secondary btn-lg btn-block" data-dismiss="modal" aria-label="Close">
+            Close
+          </button>
+        </div>
+      </div>
+    )
   }
 
   renderModal() {
     return (
       <div className="modal fade" id="submitModal" ref="submitModal" tabIndex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
         <div className="modal-dialog" role="document">
-          <div className="modal-content">
-            <div className="modal-header">
-              <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-              </button>
-              <h4 className="modal-title" id="myModalLabel">Recommend Stuff</h4>
-              <p>Stuff can be an app, a package, an API or anything else that adds functionality to an IoT device.</p>
-            </div>
-            <form onSubmit={this.handleSubmit.bind(this)}>
-              <div className="modal-body">
-                  <h5 className='m-t-1'>Details</h5>
-                  <div className="form-group">
-                    <label htmlFor="name">Name</label>
-                    <input type="text" className="form-control" id="name" aria-describedby="Name" />
-                  </div>
-
-                  <div className="form-group">
-                    <label htmlFor="description">Description</label>
-                    <textarea className="form-control" id="description" rows="5"></textarea>
-                  </div>
-
-                  <div className="form-group">
-                    <label htmlFor="compnay">Company</label>
-                    <input type="text" className="form-control" id="company" aria-describedby="company" />
-                  </div>
-
-                  <div className="form-group">
-                    <label htmlFor="iconUrl">Icon URL</label>
-                    <input type="text" className="form-control" id="iconUrl" aria-describedby="IconURL" />
-                  </div>
-
-                  <h5>Compatible IoT</h5>
-                  <ThingsCheckboxes />
-
-                  <h5 className="m-t-1">Runs on</h5>
-                  <ControllersCheckboxes />
-
-                  <h5 className="m-t-1">Tags</h5>
-                  <KindsCheckboxes />
-              </div>
-              <div className="modal-footer">
-                <button type="submit" className="btn btn-primary btn-lg btn-block" >Submit</button>
-              </div>
-            </form>
-          </div>
+          { this.props.showSuccess ? this.renderSuccess() : this.renderForm() }
         </div>
       </div>
     )
@@ -130,3 +166,12 @@ export default class Footer extends Component {
     )
   }
 }
+
+export default createContainer(({}) => {
+  Session.setDefault(SHOW_SUCCESS, false)
+  const showSuccess = Session.get(SHOW_SUCCESS)
+
+  return {
+    showSuccess,
+  };
+}, Footer);
