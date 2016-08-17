fbbb1528f9ac885ab01e103cf593bb4ee7797b87
Marvin Arnold <marvinmarnold@gmail.com>
2016-08-17 18:10:47 -0500

    Add some styles to geeky.rocks base

--- imports/ui/pages/IndexPage.jsx
+++ imports/ui/pages/IndexPage.jsx
@@ -26,12 +26,14 @@ export default class GeekyIndexPage extends Component {
 
   renderProjects() {
     return this.getProjects().map((project) => (
-      <div key={project._id} className="col-xs-6 col-lg-4">
-        <div className="card card-block">
-          <h4 className="card-title">{project.name}</h4>
-          <p className="card-text">{project.description}</p>
-          <a href={project.url} className="card-link">Learn More</a>
-        </div>
+      <div key={project._id} className="col-xs-12 col-md-6 col-lg-4 col-xl-6">
+        <a href={project.url} className="geeky-card">
+          <div className="card card-block project-card geeky-card">
+            <h4 className="card-title">{project.name}</h4>
+            <p className="card-text geeky-text m-t-1">{project.description}</p>
+            <a href={project.url} className="card-link geeky-text m-t-2">Learn More</a>
+          </div>
+        </a>
       </div>
     ));
   }
@@ -39,11 +41,11 @@ export default class GeekyIndexPage extends Component {
   render() {
     return (
       <div className="container">
-        <div className="m-t-2">
-          <h1>Projects</h1>
+        <div className="p-t-2">
+          <h1 className="p-b-2 geeky-title">Projects</h1>
         </div>
 
-        <div className="row">
+        <div className="row p-b-3">
           {this.renderProjects()}
         </div>
       </div>
