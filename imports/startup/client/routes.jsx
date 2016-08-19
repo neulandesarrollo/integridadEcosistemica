2d6e3274c4db2f91279d1ff552d2155b81c75c0f
Marvin Arnold <marvinmarnold@gmail.com>
2016-08-19 17:13:33 -0500

    Track every page with Piwik

--- imports/startup/client/routes.jsx
+++ imports/startup/client/routes.jsx
@@ -2,33 +2,21 @@ import React from 'react';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { mount } from 'react-mounter';
 
+import { piwik } from 'meteor/marvin:piwik-http-sandstorm';
+
 import '../../ioplea.se/startup/client/routes.jsx';
 import '../../mexEco/startup/client/routes.jsx';
 
 import AppContainer from '../../ui/containers/AppContainer.jsx';
 import IndexPage from '../../ui/pages/IndexPage.jsx';
 
+FlowRouter.triggers.enter([piwik.flowTrackEnter]);
+
 FlowRouter.route('/', {
   name: 'index',
-  triggersEnter: [trackRouteEntry],
   action() {
     mount(AppContainer, {
       main: <IndexPage/>,
     });
   },
 });
-
-function trackRouteEntry(context) {
-  HTTP.get(Meteor.settings.public.PIWIK.url, {
-    headers: {"Authorization": "Bearer " + Meteor.settings.public.PIWIK.sandstormAuth},
-    params: {
-      idsite: 1,
-      rec: 1,
-      url: "https://www.geeky.rocks/joe",
-    }
-  }, (err, resp) => {
-    console.log('posted');
-    console.log(err);
-    console.log(resp);
-  })
-}
