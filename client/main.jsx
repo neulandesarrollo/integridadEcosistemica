5ec94d0f0971594b4a24ca577660dccb5581e7a3
Marvin Arnold <marvinmarnold@gmail.com>
2016-08-18 19:28:08 -0500

    Add attributes to <html> and <head> for better Bootstrap support

--- client/main.jsx
+++ client/main.jsx
@@ -1,3 +1,4 @@
 import '../imports/startup/client/routes.jsx';
+import '../imports/startup/client/bootstrap.js';
 
 import { startupMexEcoClient } from '../imports/mexEco/startup/client/startup.js';
---
+++ imports/startup/client/bootstrap.js
@@ -0,0 +1,3 @@
+Meteor.startup(() => {
+   $('html').attr('lang', 'en');
+});
