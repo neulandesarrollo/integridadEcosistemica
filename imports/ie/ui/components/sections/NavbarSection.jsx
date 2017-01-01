3d65f8f1838dc5b0aa83350b41aacce22d7bbd43
Marvin Arnold <marvin@unplugged.im>
2017-01-01 22:18:52 +0000

    Responsive font size with bootstrap. IE: Navbar styled.

--- imports/ie/ui/components/sections/NavbarSection.jsx
+++ imports/ie/ui/components/sections/NavbarSection.jsx
@@ -5,12 +5,12 @@ export default class NavbarSection extends Component {
   render() {
     return (
       <nav className="navbar navbar-dark bg-inverse navbar-full">
-        <button className="navbar-toggler hidden-lg-up" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"></button>
-        <div className="collapse navbar-toggleable-md" id="navbarResponsive">
+        <button className="navbar-toggler hidden-sm-up" type="button" data-toggle="collapse" data-target="#ieNavbar" aria-controls="ieNavbar" aria-expanded="false" aria-label="Toggle navigation"></button>
+        <div className="collapse navbar-toggleable-xs" id="ieNavbar">
           <a className="navbar-brand" href="/ie">
             <img src="/ie/logo-navbar.png" className="d-inline-block align-top" alt="Integridad Ecosistemica" />
           </a>
-          <ul className="nav navbar-nav float-right">
+          <ul className="nav navbar-nav float-sm-right">
             <li className="nav-item active">
               <a className="nav-link" href="/ie">Nosotros <span className="sr-only">(current)</span></a>
             </li>
