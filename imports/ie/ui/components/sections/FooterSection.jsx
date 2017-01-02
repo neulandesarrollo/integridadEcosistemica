0a9816aa22ea39aabfe796db556f92c9f315c20b
Marvin Arnold <marvin@unplugged.im>
2017-01-02 00:13:10 +0000

    mabbox-gl-draw loading on chrome

--- imports/ie/ui/components/sections/FooterSection.jsx
+++ imports/ie/ui/components/sections/FooterSection.jsx
@@ -5,7 +5,7 @@ export default class FooterSection extends Component {
     return (
       <div className="navbar-full text-white bg-inverse py-3 px-2">
         <div className="container-fluid">
-          <div className="col-md-5">
+          <div className="col-md-5 col-sm-6">
             <span className="text-small">
 							<small>
 							Science ipsum. Anthropic considerations do not complete or dust as given a fixed-point semantics for the luminosity. Distances to have. A cosmic. Acceleration these.
@@ -13,7 +13,7 @@ export default class FooterSection extends Component {
 						</span>
           </div>
 
-          <div className="col-md-5 offset-md-2 text-xs-right">
+          <div className="col-md-5 col-sm-6 offset-md-2 text-xs-right">
 						<ul className="list-inline">
 							<li className="list-inline-item"><h2 className="px-1"><a href="#" className="text-white"><i className="icon ion-social-google"></i></a></h2></li>
 							<li className="list-inline-item"><h2 className="px-1"><a href="#" className="text-white"><i className="icon ion-social-facebook"></i></a></h2></li>
--- imports/ie/ui/components/widgets/MapboxGLDraw.jsx
+++ imports/ie/ui/components/widgets/MapboxGLDraw.jsx
@@ -1,7 +1,10 @@
 import { Meteor } from 'meteor/meteor';
 import React, { Component, PropTypes } from 'react';
-// import ReactMapboxGl from "react-mapbox-gl";
+// import MapboxGl from "mapbox-gl";
 // import Draw from 'mapbox-gl-draw';
+import ReactMapboxGl from "react-mapbox-gl";
+
+import MapDraw from '@mapbox/mapbox-gl-draw';
 
 // import { polygonLineStyles } from '../../../common/mapbox-polygon-line-styles.js'
 
@@ -14,43 +17,46 @@ export default class MapboxGLDraw extends Component {
     };
   }
 
-  componentWillMount() {
-    const thiz = this;
-    const { map } = this.context;
-
-    const styles = polygonLineStyles;
-
-    const controls = {
-      point: false,
-      line_string: false,
-      polygon: true,
-      trash: false
-    };
-
-    // const draw = Draw({
-    //   controls,
-    //   styles,
-    //   position: 'bottom-right'
-    // });
-
-    map.on('load', () => {
-      // map.addControl(draw)
-    });
-
-    this.setState({draw})
-  }
-
-  componentWillUnmount() {
-    const { map } = this.context;
-
-    map.remove(this.state.draw);
-    this.setState({draw: undefined})
-  }
-
   render() {
-    return <div></div>;
+    return <div></div>
   }
 }
+
+
+  // componentWillMount() {
+  //   const thiz = this;
+  //   const { map } = this.context;
+	//
+  //   const styles = polygonLineStyles;
+	//
+  //   const controls = {
+  //     point: false,
+  //     line_string: false,
+  //     polygon: true,
+  //     trash: false
+  //   };
+	//
+  //   const draw = MapboxDraw({
+  //     controls,
+  //     styles,
+  //     position: 'bottom-right'
+  //   });
+	//
+  //   map.on('load', () => {
+  //     map.addControl(draw)
+  //   });
+	//
+  //   this.setState({draw})
+  // }
+	//
+  // componentWillUnmount() {
+  //   const { map } = this.context;
+	//
+  //   map.remove(this.state.draw);
+  //   this.setState({draw: undefined})
+  // }
+
+
 
 // MapboxGLDraw.contextTypes = {
 //   map: PropTypes.object
